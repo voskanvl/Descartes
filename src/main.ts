9dcc5ca89668b56cf25477947d85739f61f51a59
voskanvl <voskanvl@gmail.com>
2023-03-30 14:40:55 +0300

    search + burger

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,20 @@ import slides from "./slides";
 
 switchSubmenu();
 slides();
+
+const searchElement = document.querySelector<HTMLElement>(".header__search")!;
+const inputSearchElement = document.querySelector<HTMLElement>("#search-input")!;
+const headerRight = document.querySelector<HTMLElement>(".header__right")!;
+
+searchElement.addEventListener("mouseenter", () => {
+    if (innerWidth > 1246) return;
+    // headerRight.style.width = "40vw";
+    inputSearchElement.style.width = "240px";
+    inputSearchElement.style.opacity = "1";
+});
+searchElement.addEventListener("mouseleave", () => {
+    if (innerWidth > 1246) return;
+    headerRight.style.width = "";
+    inputSearchElement.style.width = "";
+    inputSearchElement.style.opacity = "0";
+});
