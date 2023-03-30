c726336650bde04f73b93ec504d1a50dd15f023c
voskanvl <voskanvl@gmail.com>
2023-03-30 10:42:59 +0300

    catalog

---
+++ src/components/main-menu/main-menu.ts
@@ -0,0 +1,73 @@
+export default function switchSubmenu() {
+    const mainMenuItems = [...document.querySelectorAll<HTMLElement>(".main-menu__item")];
+    const mainSubmenus = [...document.querySelectorAll<HTMLElement>(".main-menu__submenu")];
+    const mainMenuList = document.querySelector<HTMLElement>(".main-menu__list");
+
+    const MenuSubmenuArray = mainMenuItems
+        .map(e => {
+            const submenu = mainSubmenus.find(v => e.dataset.id === v.dataset.id)!;
+            if (e.dataset.id === undefined)
+                return {
+                    id: null,
+                    menu: null,
+                    submenu: null,
+                };
+            return {
+                id: e.dataset.id,
+                menu: e,
+                submenu,
+            };
+        })
+        .filter(e => e !== undefined);
+
+    if (!MenuSubmenuArray || !MenuSubmenuArray.length)
+        throw Error("проблемы с сопоставлением  .main-menu__item .main-menu__submenu");
+
+    type MenuSubmenuType = Record<string, { menu: HTMLElement; submenu: HTMLElement }>;
+
+    let MenuSubmenuMap: MenuSubmenuType | {} = {};
+
+    for (const { id, menu, submenu } of MenuSubmenuArray) {
+        if (!id || !menu || !submenu) continue;
+        if (!MenuSubmenuMap) {
+            MenuSubmenuMap = { [id]: { menu, submenu } };
+        } else {
+            MenuSubmenuMap = { ...MenuSubmenuMap, [id]: { menu, submenu } };
+        }
+    }
+
+    const isMenuSubmenuType = (x: MenuSubmenuType): x is MenuSubmenuType => {
+        if (
+            Object.keys(x).length > 0 &&
+            "menu" in Object.values(x)[0] &&
+            "submenu" in Object.values(x)[0]
+        )
+            return true;
+        return false;
+    };
+
+    if (!MenuSubmenuMap)
+        throw Error("проблемы с сопоставлением  .main-menu__item .main-menu__submenu");
+
+    mainMenuList &&
+        mainMenuList.addEventListener("mousemove", (event: Event) => {
+            const target = event.target as HTMLElement;
+            const closest = target.closest(".main-menu__item");
+            if (!closest) return;
+            const id = (closest as HTMLElement).dataset.id!;
+            console.log(
+                "%cMyProject%cline:34%cMenuSubmenuMap",
+                "color:#fff;background:#ee6f57;padding:3px;border-radius:2px",
+                "color:#fff;background:#1f3c88;padding:3px;border-radius:2px",
+                "color:#fff;background:rgb(217, 104, 49);padding:3px;border-radius:2px",
+                MenuSubmenuMap,
+            );
+            mainSubmenus.forEach(e => e.removeAttribute("active"));
+            isMenuSubmenuType(MenuSubmenuMap) &&
+                (MenuSubmenuMap as MenuSubmenuType)[id].submenu.setAttribute("active", "active");
+        });
+    mainMenuList &&
+        mainMenuList.addEventListener("mouseleave", () => {
+            mainSubmenus.forEach(e => e.removeAttribute("active"));
+        });
+}
--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,8 @@
 import "./sass/style.sass";
-import { scrollScreens } from "./scrollScreens";
-import create from "zustand/vanilla";
-// import '@splidejs/splide/css';
-import cssHasPseudo from "css-has-pseudo";
-// const cssHasPseudo = require("css-has-pseudo/dist/browser");
+import "@splidejs/splide/css";
 
-export interface StoreState {
-    activeScreenNumber: number;
-    activeScreenElement: HTMLElement | null;
-    inc: () => void;
-    dec: () => void;
-    setActive: (x: number) => void;
-}
+import switchSubmenu from "./components/main-menu/main-menu";
+import slides from "./slides";
 
-const changeState = (x: number) => (state: StoreState) => ({
-    ...state,
-    activeScreenNumber: state.activeScreenNumber + x,
-});
-
-const store = create<StoreState>(set => ({
-    activeScreenNumber: 0,
-    activeScreenElement: null,
-    inc: () => set(changeState(1)),
-    dec: () => {
-        changeState(-1);
-    },
-    setActive: x => {
-        changeState(x);
-    },
-}));
-
-cssHasPseudo(document);
-scrollScreens(store);
+switchSubmenu();
+slides();
